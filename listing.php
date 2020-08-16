695d47774cc80f2bb1ac95bf727c46200b55255f
Jack Wohl <jnw900@gmail.com>
2020-08-15 21:04:57 -0400

    changed styles directory to css

--- listing.php
+++ listing.php
@@ -9,8 +9,7 @@
   <meta name='viewport' content='width=device-width, initial-scale=1'>
   <!-- Ensures optimal rendering on mobile devices. -->
   <meta http-equiv='X-UA-Compatible' content='IE=edge' /> <!-- Optimal Internet Explorer compatibility -->
-  <link rel='stylesheet' type='text/css' href='../styles/listing.css'>
-  <link rel='stylesheet' type='text/css' href='../styles/common.css'>
+  <link rel='stylesheet' type='text/css' href='../css/common.css'>
 </head>
     
 <body>
