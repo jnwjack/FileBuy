ea5fa92cb9c3c2b44c6e1218c3b5224285058861
Jack Wohl <jnw900@gmail.com>
2020-08-23 16:17:01 -0400

    name and size on listing page

--- js/util.js
+++ js/util.js
@@ -6,8 +6,18 @@ function isImage(file) {
 }
 
 function isEmailAddress(string) {
-  if(string && string.match("^[^@]+@[^@]+\.[^@]+$")) {
+  if (string && string.match("^[^@]+@[^@]+\.[^@]+$")) {
     return true;
   }
   return false;
+}
+
+function formatBytes(number, unitIndex = 0) {
+  units = ['bytes', 'KB', 'GB']
+  if(number < 1000 || unitIndex === 3) {
+    return number.toString(10) + ' ' + units[unitIndex];
+  }
+  let nextNumber = number / 1000.0;
+  nextNumber.toFixed(2);
+  return formatBytes(nextNumber, ++unitIndex);
 }
