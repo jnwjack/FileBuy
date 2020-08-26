82d3638ea067cb6a96adbf211beb75ccf6d7ecde
Jack Wohl <jnw900@gmail.com>
2020-08-26 14:31:41 -0400

    download button for already-paid-for listings

--- php/download.php
+++ php/download.php
@@ -8,16 +8,13 @@
   $db = new PDO('mysql:host=localhost;dbname=file_buy', $username, $password,
   array(PDO::ATTR_EMULATE_PREPARES => false, PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 
-  $statement = $db->prepare('SELECT file, order_id FROM listings WHERE id=:id');
+  $statement = $db->prepare('SELECT file, order_id, complete FROM listings WHERE id=:id');
   $statement->bindValue(':id',$_POST['listing'],PDO::PARAM_INT);
   $statement->execute();
   $row = $statement->fetch(PDO::FETCH_ASSOC);
   $order_id = $row['order_id'];
 
-  if($order_id != $_POST['order']) {
-    echo 'FAILURE: INVALID ORDER';
-  }
-  else {
+  if($order_id == $_POST['order'] || $row['complete'] == 1) {
     $ch = curl_init("https://api.sandbox.paypal.com/v2/checkout/orders/$order_id");
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HTTPHEADER, array(
@@ -33,8 +30,18 @@
     else {
       $file = unserialize($row['file']);
       $jsonData = json_encode($file);
+
+      if($row['complete'] == 0) {
+        $update = $db->prepare('UPDATE listings SET complete = 1 WHERE id=:id');
+        $update->bindValue(':id',$_POST['listing'],PDO::PARAM_INT);
+        $update->execute();
+      }
+
       echo $jsonData;
     }
+  }
+  else {
+    echo 'FAILURE: INVALID ORDER';
   }
 
 ?>
