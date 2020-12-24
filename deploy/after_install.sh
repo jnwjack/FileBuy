d200cc8f4489de581c0ee2a785152760d2c3c6f2
Jack Wohl <jnw900@gmail.com>
2020-12-24 02:41:09 -0500

    fixed file path

--- deploy/after_install.sh
+++ deploy/after_install.sh
@@ -2,5 +2,5 @@
 cd /home/bitnami/source/FileBuy
 git pull
 git secret reveal -f -p $SECRET_PWD
-sudo cp auth/CLIENT_ID ../../htdoc/auth/CLIENT_ID
-sudo cp	auth/CLIENT_SECRET ../../htdoc/auth/CLIENT_SECRET
+sudo cp auth/CLIENT_ID /opt/bitnami/apache/htdocs/auth/CLIENT_ID
+sudo cp	auth/CLIENT_SECRET /opt/bitnami/apache/htdocs/auth/CLIENT_SECRET
