c1e338578972c731ac024b025d39f01a2c535fdb
Jack Wohl <jnw900@gmail.com>
2021-01-03 01:08:19 -0500

    redesigned preview

--- js/preview.js
+++ js/preview.js
@@ -4,11 +4,11 @@
 
 */
 function defaultPreview() {
-  let canvas = document.getElementById('preview');
-  let context = canvas.getContext('2d');
+  let canvas = document.getElementById("preview");
+  let context = canvas.getContext("2d");
 
-  canvas.className = '';
-  context.font = '22px serif';
+  canvas.className = "";
+  context.font = "22px serif";
 
   let width = canvas.width;
   let height = canvas.height;
@@ -18,17 +18,17 @@ function defaultPreview() {
 
   for (let i = 0; i < numBlocks; i++) {
     for (let j = 0; j < numBlocks; j++) {
-      let color = ((i + j) % 2 == 0) ? 'rgb(191, 191, 191)' : 'rgb(255, 255, 255)';
+      let color = ((i + j) % 2 == 0) ? "rgb(191, 191, 191)" : "rgb(255, 255, 255)";
       context.fillStyle = color;
       context.fillRect(i * blockWidth, j * blockHeight, blockWidth, blockHeight);
     }
   }
 
-  // context.fillStyle = 'rgb(0, 0, 0)';
-  // context.textBaseline = 'middle';
-  // context.textAlign = 'center';
-  // context.font = '3rem sans-serif';
-  // context.fillText('Preview', width / 2, height / 2);
+  // context.fillStyle = "rgb(0, 0, 0)";
+  // context.textBaseline = "middle";
+  // context.textAlign = "center";
+  // context.font = "3rem sans-serif";
+  // context.fillText("Preview", width / 2, height / 2);
 }
 
 /* savePreviewAsBase64()
@@ -68,7 +68,42 @@ function savePreviewAsBlob() {
 */
 function enablePreviewCard() {
   let previewCard = document.getElementById("preview-card");
-  if(previewCard.className.indexOf('disabled') !== -1) {
+  if(previewCard.className.indexOf("disabled") !== -1) {
     previewCard.className = "";
   }
+
+  let content = document.getElementById('content');
+  content.className = 'blur';
+
+  let inputs = Array.from(document.getElementsByTagName('input'));
+  let buttons = Array.from(document.getElementsByTagName('button'));
+  let elementArray = inputs.concat(buttons);
+  elementArray.forEach((input) => {
+    if(input.parentElement !== previewCard) {
+      input.disabled = true;
+    }
+  });
+
+  let fileInput = document.getElementById('file');
+  fileInput.className = 'file-button';
+}
+
+function disablePreviewCard() {
+  let previewCard = document.getElementById("preview-card");
+  if(previewCard.className.indexOf("disabled") === -1) {
+    previewCard.className = "disabled"
+  }
+
+  let content = document.getElementById('content');
+  content.className = '';
+
+  let inputs = Array.from(document.getElementsByTagName('input'));
+  let buttons = Array.from(document.getElementsByTagName('button'));
+  let elementArray = inputs.concat(buttons);
+  elementArray.forEach((input) => {
+      input.disabled = false;
+  });
+
+  let fileInput = document.getElementById('file');
+  fileInput.className = 'file-button hoverable';
 }
