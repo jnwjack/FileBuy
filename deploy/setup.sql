af037ab633d22ea3911a94f1961796f54dd77fb2
Jack Wohl <jnw900@gmail.com>
2021-02-28 21:48:49 -0500

    database script

---
+++ deploy/setup.sql
@@ -0,0 +1,43 @@
+/* setup.sql
+
+  Set up database and tables.
+  Run this: mysql -u username -p < setup.sql
+  Or, from within the MySQL CLI: source /path/to/setup.sql
+
+*/
+
+CREATE DATABASE IF NOT EXISTS file_buy;
+USE file_buy;
+
+CREATE TABLE IF NOT EXISTS listings (
+  preview BLOB DEFAULT NULL,
+  email VARCHAR(100) NOT NULL,
+  price DECIMAL(13,2) NOT NULL,
+  id INT(11) UNSIGNED NOT NULL,
+  order_id char(17) DEFAULT NULL,
+  name varchar(100) NOT NULL,
+  size int(15) UNSIGNED NOT NULL,
+  complete tinyint(2) NOT NULL,
+  time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY ( id )
+);
+
+CREATE TABLE IF NOT EXISTS commissions (
+  id INT(11) UNSIGNED NOT NULL,
+  steps TINYINT(4) UNSIGNED NOT NULL DEFAULT 1,
+  email VARCHAR(100) NOT NULL,
+  current INT(4) UNSIGNED NOT NULL DEFAULT 1,
+  PRIMARY KEY ( id )
+);
+
+CREATE TABLE IF NOT EXISTS steps (
+  commission_id INT(11) UNSIGNED NOT NULL,
+  sequence_number TINYINT(4) NOT NULL,
+  price DECIMAL(13,2) NOT NULL,
+  preview BLOB DEFAULT NULL,
+  name VARCHAR(100) DEFAULT NULL,
+  status TINYINT(3) NOT NULL DEFAULT 0,
+  title VARCHAR(100) NOT NULL DEFAULT "Milestone",
+  description TEXT NOT NULL DEFAULT "No description provided.",
+  order_id char(17) DEFAULT NULL
+);
