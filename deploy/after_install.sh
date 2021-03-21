b4b30079d75161709f2ff9af03fffdf82068323c
Jack Wohl <jnw900@gmail.com>
2021-03-20 21:02:17 -0400

    fixed composer issue maybe

--- deploy/after_install.sh
+++ deploy/after_install.sh
@@ -4,3 +4,5 @@ git pull
 git secret reveal -f -p $SECRET_PWD
 sudo cp auth/CLIENT_ID /opt/bitnami/apache/htdocs/auth/CLIENT_ID
 sudo cp	auth/CLIENT_SECRET /opt/bitnami/apache/htdocs/auth/CLIENT_SECRET
+cd /opt/bitnami/apache/htdocs
+composer update
