633f253cc9e5c1e2d99729ccece1ce7dc325a08f
Jack Wohl <jnw900@gmail.com>
2021-03-26 18:49:11 -0400

    about page and contact page

--- deploy/after_install.sh
+++ deploy/after_install.sh
@@ -5,4 +5,3 @@ git secret reveal -f -p $SECRET_PWD
 sudo cp auth/CLIENT_ID /opt/bitnami/apache/htdocs/auth/CLIENT_ID
 sudo cp	auth/CLIENT_SECRET /opt/bitnami/apache/htdocs/auth/CLIENT_SECRET
 cd /opt/bitnami/apache/htdocs
-composer update
