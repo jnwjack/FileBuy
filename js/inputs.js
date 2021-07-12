89740b79807ba95f0822f6e74c8d8d7c25d905d7
Jack Wohl <jnw900@gmail.com>
2021-07-12 00:00:14 -0400

    improved price input fields

--- js/inputs.js
+++ js/inputs.js
@@ -2,3 +2,17 @@ function toggleButtonProgressBar(active) {
   document.getElementById('submit-button-text').classList.toggle('disabled', active);
   document.getElementById('progress-bar').classList.toggle('invisible', !active);
 }
+
+/* priceInputCallback(event)
+
+  Called when any input is made to a price field. Ensures that any value has only 2
+  decimal points
+
+*/
+function priceInputCallback(event) {
+  if(event.srcElement.valueAsNumber > 1000) {
+    event.srcElement.valueAsNumber = 1000;
+  } else {
+    event.srcElement.valueAsNumber = formatPrice(event.srcElement.valueAsNumber);
+  }
+}
