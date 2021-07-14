b42f723b8251bcee8c3bb59bd2639d1543cdf7ab
Jack Wohl <jnw900@gmail.com>
2021-07-14 02:16:34 -0400

    add evidence backend

---
+++ php/commission_add_evidence.php
@@ -0,0 +1,85 @@
+<?php
+
+  /* commission_add_evidence.php
+
+    Submit file to be used as evidence for a certain step in the
+    commission. Up to 3 files can be used per step.
+
+  */
+
+  require_once('database_request.php');
+  require_once('paypal_request.php');
+  require_once('util.php');
+
+  $commission_id = $_POST['commission'];
+  $file = serialize($_POST["file"]);
+
+  if(fileTooLarge($file)) {
+    http_response_code(413);
+    die('FAILURE: File too large');
+  }
+
+  $db = getDatabaseObject();
+
+  $commissionStatement = $db->prepare('SELECT current, email, steps FROM commissions WHERE id=:id');
+  $commissionStatement->bindValue(':id', $commission_id, PDO::PARAM_STR);
+  $successful = $commissionStatement->execute();
+  if(!$successful) {
+    http_response_code(500);
+    die('FAILURE: Could not fetch commission');
+  }
+  $commissionStatementRow = $commissionStatement->fetch(PDO::FETCH_ASSOC);
+  $currentStepNumber = $commissionStatementRow['current'];
+  $totalSteps = $commissionStatementRow['steps'];
+  $email = $commissionStatementRow['email'];
+
+  $stepStatement = $db->prepare('SELECT * FROM steps WHERE commission_id=:commission_id AND sequence_number=:sequence_number');
+  $stepStatement->bindValue(':commission_id', $commission_id, PDO::PARAM_STR);
+  $stepStatement->bindValue(':sequence_number', $currentStepNumber, PDO::PARAM_INT);
+  $successful = $stepStatement->execute();
+  if(!$successful) {
+    http_response_code(500);
+    die('FAILURE: Could not fetch milestone');
+  }
+  $stepStatementRow = $stepStatement->fetch(PDO::FETCH_ASSOC);
+  $currentStepEvidenceCount = $stepStatementRow['evidence_count'];
+  $currentStepStatus = $stepStatementRow['status'];
+  if($currentStepEvidenceCount >= 3) {
+    http_response_code(500);
+    die('FAILURE: No room for more evidence for this milestone');
+  }
+  // Can't add evidence if payment has already been made.
+  if($currentStepStatus > 1) {
+    http_response_code(500);
+    die('FAILURE: Payment already made for this milestone');
+  }
+
+  $newEvidenceCount = $currentStepEvidenceCount + 1;
+  $fileWriteSuccessful = file_put_contents("/opt/data/${commission_id}-${currentStepNumber}-e${newEvidenceCount}", $file);
+  if(!$fileWriteSuccessful) {
+    http_response_code(500);
+    die('FAILURE: Could not write file');
+  }
+
+  // Update evidence count
+  $updateStatement = $db->prepare('UPDATE steps SET evidence_count=:evidence_count');
+  $updateStatement->bindValue(':evidence_count', $newEvidenceCount, PDO::PARAM_INT);
+  $successful = $updateStatement->execute();
+  if(!$successful) {
+    http_response_code(500);
+    die('FAILURE: Could not update new status of commission');
+  }
+
+  // Fetch file and encode it as json object
+  $file = unserialize(file_get_contents("/opt/data/$commissionID-$stepNumber-e${newEvidenceCount}"));
+  $returnData = array(
+    'current' => $currentStepNumber,
+    'commission' => $commission_id,
+    'evidenceCount' => $newEvidenceCount,
+    'newEvidence' => $file
+  );
+  $jsonData = json_encode($returnData);
+
+  echo $jsonData;
+
+?>
