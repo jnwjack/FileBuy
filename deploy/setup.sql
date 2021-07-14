c14e65d2f9d9dc509e82a1049f5b0c5e983223d0
Jack Wohl <jnw900@gmail.com>
2021-07-14 02:16:34 -0400

    add evidence backend

--- deploy/setup.sql
+++ deploy/setup.sql
@@ -17,14 +17,14 @@ CREATE TABLE IF NOT EXISTS listings (
   order_id char(17) DEFAULT NULL,
   name varchar(100) NOT NULL,
   size int(15) UNSIGNED NOT NULL,
-  complete tinyint(2) NOT NULL DEFAULT 0,
+  complete tinyint(1) NOT NULL DEFAULT 0,
   time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY ( id )
 );
 
 CREATE TABLE IF NOT EXISTS commissions (
   id VARCHAR(36),
-  steps TINYINT(4) UNSIGNED NOT NULL DEFAULT 1,
+  steps TINYINT(1) UNSIGNED NOT NULL DEFAULT 1,
   email VARCHAR(100) NOT NULL,
   current INT(4) UNSIGNED NOT NULL DEFAULT 1,
   PRIMARY KEY ( id )
@@ -32,12 +32,13 @@ CREATE TABLE IF NOT EXISTS commissions (
 
 CREATE TABLE IF NOT EXISTS steps (
   commission_id VARCHAR(36),
-  sequence_number TINYINT(4) NOT NULL,
+  sequence_number TINYINT(1) NOT NULL,
   price DECIMAL(13,2) NOT NULL,
   preview BLOB DEFAULT NULL,
   name VARCHAR(100) DEFAULT NULL,
-  status TINYINT(3) NOT NULL DEFAULT 0,
+  status TINYINT(1) NOT NULL DEFAULT 0,
   title VARCHAR(100) NOT NULL DEFAULT "Milestone",
   description TEXT NOT NULL,
-  order_id char(17) DEFAULT NULL
+  order_id char(17) DEFAULT NULL,
+  evidence_count TINYINT(1) NOT NULL DEFAULT 0
 );
