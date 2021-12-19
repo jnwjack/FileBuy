ac4f9e5c31752e50a7a0a03903d5350b2d4fbf11
Jack Wohl <jnw900@gmail.com>
2021-12-19 15:03:49 -0500

    merge changes from master

--- 404.php
+++ 404.php
@@ -13,11 +13,6 @@
 </head>
 
 <body>
-<<<<<<< HEAD:404.html
-  <div class='content'>
-      <div class='content-part'>
-        Basic 404 page
-=======
   <?php
     include_once('php/view/header.php');
     include_once('php/view/side_menu.php');
@@ -27,7 +22,6 @@
       <div>
         <img id='notfound' src='images/404.png' alt='File icon with question mark'>
         <p>Sorry, that page can't be found. If you're looking for a listing or commission, it might have been removed.</p>
->>>>>>> c5bee2e27ed8fb64f49618a54823ee3c3b8a34fd:404.php
       </div>
   </div>
 </body>
